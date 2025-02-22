7e922d6566711bc2741b8f95d28b6e6bdb38ae6f
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-02-22 23:40:24 +0530

    inital commit on basic1

---
+++ src/main/java/P5.java
@@ -0,0 +1,14 @@
+
+
+public class P5 {
+	
+	public static void main(String[] args) {
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("*");
+		System.out.print("*");
+		System.out.println("*");
+		System.out.print("*");
+	}
+
+}
---
+++ src/main/java/p7.java
@@ -0,0 +1,9 @@
+
+
+public class p7 {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("1,2");
+	}
+}
