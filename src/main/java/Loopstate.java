975fe9d854b5c42728cffede1062f4a7c87bcf58
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-02-23 20:53:53 +0530

    basic2

---
+++ src/main/java/Loopstate.java
@@ -0,0 +1,48 @@
+
+public class Loopstate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * Loop Statments:
+		 *          while
+		 *          do while
+		 *          for 
+		 *          advanced for(for each)**/
+		
+		
+		// while
+		/**
+		 * while(Condition)
+		 * {
+		 *  statement;
+		 *  }
+		 *  
+		 *  **/
+		
+		
+		// do while 
+		
+		/**
+		 * do
+		 * {
+		 * stmts;
+		 * }
+		 * while(condition);**/
+		
+	
+		
+		int i=5;
+		do 
+		{
+			//System.out.println(i); //op5
+			System.out.println(++i); //5 i=6
+			System.out.println(i);
+		}
+		while(i<=8);
+		
+
+	}
+
+}
---
+++ src/main/java/SwitchCases.java
@@ -0,0 +1,29 @@
+
+public class SwitchCases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * switch(exp/value){
+		 *  case value1: stmts;
+		 *               break;
+		 *  case value2: 
+		 *     stmts;
+		 *     break;
+		 *  default:
+		 *     stmts;
+		 *     
+		 *         }**/
+		
+		
+		//////////////////////////////////////
+		
+		/**
+		 * Break:
+		 *  break is a control transfer statments
+		 *  break statment is used transfer the controller outside of the switch block or loop block
+		 *  ***/
+
+	}
+
+}
