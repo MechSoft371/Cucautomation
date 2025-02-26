a7c8942e275718a654504f0f125da66d2a167e22
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-02-26 22:22:24 +0530

    array2basic

---
+++ src/main/java/Array2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// how to take input from keyboard/ at run time?
+		// scanner is class which contains a number of the methods
+		
+//		Scanner sc= new Scanner(System.in);
+//		int a =sc.nextInt();
+//		System.out.println(a);
+//		
+//		System.out.println("enter the d");
+//		double d= sc.nextDouble();
+//		System.out.println("given decimal number:"+d);
+//		
+//		System.out.println("enter the string");
+//		String s= sc.next();
+//		System.out.println("Given string:"+s);
+		
+
+		// searching an element in array
+		// find the a number in array
+		// find the number of even and odd in array
+		// sort elements in array using built in methods
+		// reverse an array
+		// find the largest and lowest number in array
+		// find the missing number in array
+		// sort the element in array using loops
+		
+		 
+		
+	}
+
+}
