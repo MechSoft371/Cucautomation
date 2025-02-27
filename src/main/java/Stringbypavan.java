46391bc07e6f62c21a7bc2cedc5b76d4b2cd6f5a
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-02-27 18:21:50 +0530

    Stringprobypavansir

---
+++ src/main/java/Stringbypavan.java
@@ -0,0 +1,22 @@
+
+public class Stringbypavan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name= "Prudhvi";
+		String lastname=new String("raj");
+		// String is predefined class
+		//it is not a primitive datatype
+		//String is a collect of characters
+		
+		
+		System.out.println(name.charAt(name.length()-1));
+		System.out.println(name.compareTo(lastname));
+		System.out.println(name.concat(lastname));
+		System.out.println(name.length());
+		System.out.println(name.replace('P', 'p'));
+		
+
+	}
+
+}
