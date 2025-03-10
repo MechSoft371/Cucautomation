70bb705f329e9712a777342038fda905f6c1e278
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-03-10 14:07:39 +0530

    methodsandobject

---
+++ src/main/java/mainmeth.java
@@ -0,0 +1,17 @@
+
+public class mainmeth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		meth m=new meth();
+		m.m1();
+		String s=m.m2();
+		System.out.println(s);
+		m.m3("prudhvi");
+		System.out.println(m.m4("raj"));
+		s.length();
+		System.out.println(s.substring(2, s.length()-1));
+
+	}
+
+}
---
+++ src/main/java/meth.java
@@ -0,0 +1,37 @@
+
+public class meth {
+	
+	/*methods::
+	 * method is block of code, here we have user define methods and predefined method*/
+	
+	
+	//no parm no return value
+	void m1() {
+		System.out.println("Hello");
+	}
+	
+	// no param have return value
+	
+	String m2() {
+		return("Hello how are you");
+	}
+	
+	//take paras no return value
+	void m3(String s) {
+		System.out.println("Hello "+s);
+	}
+	
+	//take params have return value
+
+	String m4(String s) {
+		return ("Hello "+s);
+		
+	}
+	
+	/*Constructor:
+	 * 1.constructor should have same as class name
+	 * 2. constructor will never return a value
+	 * 
+	 * at the time of object creation construct is called and assign the value
+	 * but we have to call method explictly by using object reference variable  */
+}
