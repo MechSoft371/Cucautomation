9cb7250c67e7ba8cb74213b7ea68735ddc99b820
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-03-15 15:51:13 +0530

    changes

---
+++ src/main/java/oops/encapsulation.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class encapsulation {
+	//this keywords
+	
+	//where we have use this keyword
+	int x,y; //class or instance variable
+	
+	void setData(int a, int b) {
+		x=a;
+		y=b; 
+	}
+	
+	public static void main(String[] args) {
+		//when we create any variable inside the method body is called local variable
+		
+	}
+
+}
