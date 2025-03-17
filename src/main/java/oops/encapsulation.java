3cbd593da75dd5384f653c8bbb798452a4d1d126
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2025-03-17 21:36:29 +0530

    startedencapul

--- src/main/java/oops/encapsulation.java
+++ src/main/java/oops/encapsulation.java
@@ -4,15 +4,27 @@ public class encapsulation {
 	//this keywords
 	
 	//where we have use this keyword
-	int x,y; //class or instance variable
+	static int x; //class or instance variable
+	static int y;
 	
-	void setData(int a, int b) {
-		x=a;
-		y=b; 
+	void setData(int x, int y) {//here x,y are local variables
+		//  in this case when local variables and class variable same, how exceuor will understand the  which x is assigning the value?
+		// this keyword represent the class name
+		this.x=x;
+		this.y=y; 
 	}
+	
+	void display() {
+		System.out.println(x +" ,"+y);
+	}
+	
 	
 	public static void main(String[] args) {
 		//when we create any variable inside the method body is called local variable
+		
+		encapsulation e=new encapsulation();
+		e.setData(4, 5);
+		e.display();
 		
 	}
 
